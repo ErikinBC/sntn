e0502acd147072cf7beeb3705fc70a520d9502ce
Erik Drysdale <alla_hack@hotmail.com>
2023-04-07 10:45:02 -0400

    fit method working to estimate true mean for large enough sample

--- sntn/dists.py
+++ sntn/dists.py
@@ -6,11 +6,11 @@ Contains main distributions (i.e. SNTN)
 import numpy as np
 from scipy.stats import truncnorm
 # Internal modules
-from sntn.utilities.utils import rvec, cvec, vprint, broastcast_max_shape
+from sntn.utilities.utils import vprint, broastcast_max_shape
 
 
 class tnorm():
-    def __init__(self, mu:float or np.ndarray or int, sigma2:float or np.ndarray or int, a:float or np.ndarray or int, b:float or np.ndarray or int) -> None:
+    def __init__(self, mu:float or np.ndarray or int, sigma2:float or np.ndarray or int, a:float or np.ndarray or int, b:float or np.ndarray or int, verbose:bool=False) -> None:
         """
         Main model class for the truncated normal distribution
 
@@ -20,6 +20,7 @@ class tnorm():
         sigma2:             Variance of the truncated normal (can be array)
         a:                  Lower bounds of truncated normal (can be -infty)
         b:                  Upperbounds of truncated normal (can be +infty)
+        verbose:            Whether printing will occur some methods
 
         Attributes
         ----------
@@ -47,19 +48,68 @@ class tnorm():
         # Initialize the distribution
         self.dist = truncnorm(loc=self.mu, scale=self.sigma, a=self.alpha, b=self.beta)
 
-    def cdf(self, x):
+    def cdf(self, x:np.ndarray) -> np.ndarray:
         """Wrapper for scipy.stats.truncnorm(...).cdf()"""
         return self.dist.cdf(x)
 
-    def ppf(self, x):
+    def ppf(self, x:np.ndarray) -> np.ndarray:
         """Wrapper for scipy.stats.truncnorm(...).ppf()"""
         return self.dist.ppf(x)
 
-    def pdf(self, x):
+    def pdf(self, x:np.ndarray) -> np.ndarray:
         """Wrapper for scipy.stats.truncnorm(...).pdf()"""
         return self.dist.pdf(x)
 
-    def rvs(self, n, seed=None):
+    def fit(self, x:np.ndarray, use_a:bool=True, use_b:bool=True, use_sigma:bool=False) -> tuple:
+        """
+        Wrapper for the fit argument (assumes a/b are constant). Order is: a,b,mu,sigma
+
+        Parameters
+        ----------
+        x:              Data where the first axis is the samples (i.e. x[:,0,0,..] should be of len(nsamp))
+        use_a:          Whether the lowerbound should be used from initialization
+        use_b:          Whether the upperbound should be used from initialization
+        use_sigma:      Whether the variance should be used from initialization
+
+        Returns
+        -------
+        A tuple of length four, with estimates of (a_hat, b_hat, mu_hat, sigma_hat). Note that if usa_{a,b,sigma} is provided the _hat values will be the same as the values set during initialization
+        """
+        # Input prep
+        ns_x = len(x.shape)
+        fa, fb, fsigma = None, None, None
+        # Solve for vector case
+        if ns_x == 1:  # No need to loop since x is just an array
+            if use_a:
+                fa = self.alpha
+            if use_b:
+                fb = self.beta
+            if use_sigma:
+                fsigma = self.sigma
+            di = dict(zip(['fa','fb','fscale'],[fa,fb,fsigma]))
+            di = {k:v for k,v in di.items() if v is not None}
+            a_hat, b_hat, mu_hat, sigma_hat = truncnorm.fit(x, **di)
+        else:
+            # Solve for matrix case
+            a_hat = self.mu * np.nan
+            b_hat, mu_hat, sigma_hat = a_hat.copy(), a_hat.copy(), a_hat.copy()
+            # Loop over all dimensions except the first
+            for kk in np.ndindex(x.shape[1:]):
+                x_kk = x[np.s_[:,] + kk]  # Extract as array
+                if use_a:
+                    fa = self.alpha[kk]
+                if use_b:
+                    fb = self.beta[kk]
+                if use_sigma:
+                    fsigma = self.sigma[kk]
+                di = dict(zip(['fa','fb','fscale'],[fa,fb,fsigma]))
+                di = {k:v for k,v in di.items() if v is not None}
+                res_kk = truncnorm.fit(x_kk, **di)
+                a_hat[kk], b_hat[kk], mu_hat[kk], sigma_hat[kk] = res_kk
+        return a_hat, b_hat, mu_hat, sigma_hat
+
+
+    def rvs(self, n, seed=None) -> np.ndarray:
         """Wrapper for scipy.stats.truncnorm(...).rvs()"""
         # When sampling it is [num_sample,*dims of parameters]
         samp_shape = (n,)
@@ -68,56 +118,96 @@ class tnorm():
         return self.dist.rvs(samp_shape, random_state=seed)
 
 
-    def get_CI(self, x, gamma:float=0.05, lb:int or float=-1000, ub:int or float=10, nline:int=25, tol:float=1e-2, imax:int=10, verbose:bool=False):
+    # approach='root';verbose=True;alpha=0.05;kwargs={}
+    def get_CI(self, x:np.ndarray, approach:str, alpha:float=0.05, verbose:bool=False, **kwargs):
         """
-        Explain CI approach
+        Assume X ~ TN(mu, sigma, a, b), where sigma, a, & b are known. Then for any realized mu_hat (which can be estimated with self.fit()), we want to find 
+        Calculate the confidence interval for a series of points (x)
+
+        Parameters
+        ----------
+        x:                  An array-like object of points that corresponds to dimensions of estimated means
+        approach:           A total of four approaches have been implemented to calculate the CIs (see scipy.optimize.{root, minimize_scalar, minimize, root_scalar})
+
+        kwargs:             Named arguments which will be passed into the scipy.optims
+
+        Returns
+        -------
         """
-        x = rvec(x)
-        k = max(self.p, x.shape[1])
-        # Initialize
-        mu_seq = np.round(np.sinh(np.linspace(np.repeat(np.arcsinh(lb),k), 
-            np.repeat(np.arcsinh(ub),k),nline)),5)
-        q_seq = np.zeros(mu_seq.shape)
-        q_err = q_seq.copy()
-        cidx = list(range(k))
-        iactive = cidx.copy()
-        pidx = cidx.copy()
-        j, aerr = 0, 1
-        while (j<=imax) & (len(iactive)>0):
-            j += 1
-            vprint('------- %i -------' % j, verbose)
-            # Calculate quantile range
-            mus = mu_seq[:,iactive]
-            if len(iactive) == 1:
-                mus = mus.flatten()
-            if self.p == 1:
-                pidx = np.repeat(0, len(iactive))
-            elif len(iactive)==1:
-                pidx = iactive
-            else:
-                pidx = iactive
-            qs = tnorm(mus, self.sig2[pidx], self.a[pidx], self.b[pidx]).ppf(gamma)
-            if len(qs.shape) == 1:
-                qs = cvec(qs)
-            q_seq[:,iactive] = qs
-            tmp_err = q_seq - x
-            q_err[:,iactive] = tmp_err[:,iactive]
-            istar = np.argmin(q_err**2,0)
-            q_star = q_err[istar, cidx]
-            mu_star = mu_seq[istar,cidx]
-            idx_edge = (mu_star == lb) | (mu_star == ub)
-            aerr = 100*np.abs(q_star)
-            if len(aerr[~idx_edge]) > 0:
-                vprint('Largest error: %0.4f' % max(aerr[~idx_edge]),verbose)
-            idx_tol = aerr < tol
-            iactive = np.where(~(idx_tol | idx_edge))[0]
-            # Get new range
-            if len(iactive) > 0:
-                new_mu = np.linspace(mu_seq[np.maximum(0,istar-1),cidx],
-                                    mu_seq[np.minimum(istar+1,nline-1),cidx],nline)
-                mu_seq[:,iactive] = new_mu[:,iactive]
-        mu_star = mu_seq[istar, cidx]
-        # tnorm(mu=mu_star, sig2=self.sig2, a=self.a, b=self.b).ppf(gamma)
-        return mu_star
+        from scipy.optimize import root, minimize_scalar, minimize, root_scalar
+        # Input checks
+        valid_approaches = ['root', 'minimize_scalar', 'minimize', 'root_scalar']
+        assert approach in valid_approaches, f'approach needs to be one of {valid_approaches}'
+        
+        # Try to broadcast x to match underlying parameters
+        x, _ = broastcast_max_shape(x, self.mu)
+        # Define the optimization function
+        from scipy import optimize
+        optim_fun = getattr(optimize, approach)
+        
+        if approach == 'root_scale':
+            print('root_scaler')
+        elif approach == 'minimize_scaler':
+            print('minimize_scaler')
+        elif approach == 'minimize':
+            print('minimize')
+        else:
+            print('root')
+
+        # Run optimize
+        if len(kwargs) > 0:
+            res = optim_fun(1, **kwargs)
+        else:
+            res = optim_fun(1)
+        
+        
+
+
+    #     k = max(self.p, x.shape[1])
+    #     # Initialize
+    #     mu_seq = np.round(np.sinh(np.linspace(np.repeat(np.arcsinh(lb),k), 
+    #         np.repeat(np.arcsinh(ub),k),nline)),5)
+    #     q_seq = np.zeros(mu_seq.shape)
+    #     q_err = q_seq.copy()
+    #     cidx = list(range(k))
+    #     iactive = cidx.copy()
+    #     pidx = cidx.copy()
+    #     j, aerr = 0, 1
+    #     while (j<=imax) & (len(iactive)>0):
+    #         j += 1
+    #         vprint('------- %i -------' % j, verbose)
+    #         # Calculate quantile range
+    #         mus = mu_seq[:,iactive]
+    #         if len(iactive) == 1:
+    #             mus = mus.flatten()
+    #         if self.p == 1:
+    #             pidx = np.repeat(0, len(iactive))
+    #         elif len(iactive)==1:
+    #             pidx = iactive
+    #         else:
+    #             pidx = iactive
+    #         qs = tnorm(mus, self.sig2[pidx], self.a[pidx], self.b[pidx]).ppf(gamma)
+    #         if len(qs.shape) == 1:
+    #             qs = cvec(qs)
+    #         q_seq[:,iactive] = qs
+    #         tmp_err = q_seq - x
+    #         q_err[:,iactive] = tmp_err[:,iactive]
+    #         istar = np.argmin(q_err**2,0)
+    #         q_star = q_err[istar, cidx]
+    #         mu_star = mu_seq[istar,cidx]
+    #         idx_edge = (mu_star == lb) | (mu_star == ub)
+    #         aerr = 100*np.abs(q_star)
+    #         if len(aerr[~idx_edge]) > 0:
+    #             vprint('Largest error: %0.4f' % max(aerr[~idx_edge]),verbose)
+    #         idx_tol = aerr < tol
+    #         iactive = np.where(~(idx_tol | idx_edge))[0]
+    #         # Get new range
+    #         if len(iactive) > 0:
+    #             new_mu = np.linspace(mu_seq[np.maximum(0,istar-1),cidx],
+    #                                 mu_seq[np.minimum(istar+1,nline-1),cidx],nline)
+    #             mu_seq[:,iactive] = new_mu[:,iactive]
+    #     mu_star = mu_seq[istar, cidx]
+    #     # tnorm(mu=mu_star, sig2=self.sig2, a=self.a, b=self.b).ppf(gamma)
+    #     return mu_star
 
 
--- tests/test_dists_tnorm.py
+++ tests/test_dists_tnorm.py
@@ -9,33 +9,38 @@ import pytest
 import numpy as np
 from scipy.stats import norm, kstest
 # Internal
-from sntn.dists import tnorm
-from sntn.utilities.utils import has_assertion_error
 from parameters import seed
+from sntn.dists import tnorm
 
 # Set seed
 np.random.seed(seed)
 
 def gen_params(shape:tuple or list) -> tuple:
     mu = np.random.randn(*shape)
-    sigma2 = np.exp(mu)
-    a, b = mu - 2, mu + 2
+    sigma2 = np.exp(np.random.randn(*shape))
+    a = np.random.randn(*shape)
+    b = a + 2
     return mu, sigma2, a, b
 
 
-params_tnorm = [((10, )), ((10, 5)), ((10, 5, 2)),]
-@pytest.mark.parametrize("shape", params_tnorm)
+params_tnorm_rvs = [((10, )), ((10, 5)), ((10, 5, 2)),]
+@pytest.mark.parametrize("shape", params_tnorm_rvs)
 def ztest_tnorm_rvs(shape:tuple or list, nsim:int=100000, tol:float=1e-2) -> None:
     """Check that rvs() gets the mean/median we expect from theory"""
+    # Process inputs
     mu, sigma2, a, b = gen_params(shape)
     dist = tnorm(mu, sigma2, a, b)
+    ndec = int(-np.log10(tol))
+    # Check that theory lines up with underlying scipy dist
     x = dist.rvs(nsim, seed)
     Z = (norm.pdf(dist.alpha)-norm.pdf(dist.beta))/(norm.cdf(dist.beta)-norm.cdf(dist.alpha))
     mu_theory = dist.mu + Z*dist.sigma
     med_theory = dist.mu + norm.ppf((norm.cdf(dist.beta)+norm.cdf(dist.alpha))/2)*dist.sigma
-    ndec = int(-np.log10(tol))
+    assert np.all(np.abs(dist.dist.mean() - mu_theory) < 1e-10)
+    assert np.all(np.abs(dist.dist.median() - med_theory) < 1e-10)
+    # Compare to the simulated data
     err_mu = np.round(np.max(np.abs(np.mean(x, 0) - mu_theory)),ndec)
-    err_med = np.round(np.max(np.abs(np.median(x, 0) - med_theory)),ndec)
+    err_med = np.round(np.max(np.abs(np.median(x, 0) - dist.dist.median())),ndec)
     assert err_mu <= tol, f'Expected mean error to be less than {tol}: {err_mu} for {ndec} decimal places'
     assert err_mu <= tol, f'Expected median error to be less than {tol}: {err_med} for {ndec} decimal places' 
 
@@ -63,17 +68,35 @@ def ztest_tnorm_ppf(n:int=1, nsim:int=1000000, tol:float=1e-2) -> None:
     assert err_q < tol, f'QQ error was greater than {tol}: {err_q}'
 
 
+params_tnorm_fit = [((1, )), ((5,)), ((3, 2)),]
+@pytest.mark.parametrize('n', params_tnorm_fit)
+def ztest_tnorm_fit(n:int, nsim:int=100000, tol:float=1e-4) -> None:
+    """Checks that with sufficiently large samples we get point estimates"""
+    # Generate data
+    mu, sigma2, a, b = gen_params(n)
+    oracle_dist = tnorm(mu, sigma2, a, b)
+    samp = oracle_dist.rvs(nsim)
+    # Get mu_hat fit
+    _, _, mu_hat, _ = oracle_dist.fit(samp, use_a=True, use_b=True, use_sigma=True)
+    mx_err = np.max(np.abs(mu_hat - mu))
+    assert mx_err <= tol, f'Expected maximum error to be less than {tol}: {mx_err}'
+
+
 def test_tnorm_CIs() -> None:
     """Check that the confidence interval is working as expected"""
+    1
+    #breakpoint()
+
+    
 
 
 if __name__ == "__main__":
-    # Check all functions
-    for param in params_tnorm:
-        param
+    # # Check all functions
+    # for param in params_tnorm:
+    #     param
         # test_tnorm_rvs(param)
     # test_tnorm_cdf()
     # test_tnorm_ppf()
-    # test_tnorm_CIs()
+    test_tnorm_CIs()
 
     print('~~~ The test_dists.py script worked successfully ~~~')
